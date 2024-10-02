6f4d4883aae6942013e8dc2ebe013769a8b68929
Sushant0412 <mikesushant05@gmail.com>
2024-10-02 22:57:39 +0530

    added ;

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Bye")
+        System.out.println("Bye");
     }
 
     // Simple add function
